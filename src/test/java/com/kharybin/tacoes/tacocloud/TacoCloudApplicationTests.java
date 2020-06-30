668aecc079c7b00b409d5bc17708c5d0888d2bcd
Kharybin <ai.kharybin@gmail.com>
2020-06-30 14:06:49 +0300

    initial

---
+++ src/test/java/com/kharybin/tacoes/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kharybin.tacoes.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
