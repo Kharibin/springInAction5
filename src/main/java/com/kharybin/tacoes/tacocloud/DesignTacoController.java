dfdfeb6eb744501bb8647561512f337474cd15ed
Kharybin <ai.kharybin@gmail.com>
2020-07-02 20:05:00 +0300

    2.3.3 Order validation

--- src/main/java/com/kharybin/tacoes/tacocloud/DesignTacoController.java
+++ src/main/java/com/kharybin/tacoes/tacocloud/DesignTacoController.java
@@ -3,6 +3,7 @@ package com.kharybin.tacoes.tacocloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +41,7 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@Valid Taco design) {
+    public String processDesign(Taco design) {
         // Save the taco design...
         // We'll do this in chapter 3
         log.info("Processing design: " + design);
